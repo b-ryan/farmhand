e39ebd7ba302757bbd8be4e79d98c13565c832b1
Buck Ryan <buck.ryan@gmail.com>
2017-01-30 20:38:24 -0500

    use lein to run multiple test profiles

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
   :jvm-opts ["-Duser.timezone=GMT"]
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[log4j "1.2.17"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
