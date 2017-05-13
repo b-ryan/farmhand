b9f83dd2016955297fc3f9b90c817c38b62658f9
Buck Ryan <buck.ryan@gmail.com>
2017-05-13 10:56:17 -0400

    Version 0.9.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.buckryan/farmhand "0.9.1"
+(defproject com.buckryan/farmhand "0.9.2-SNAPSHOT"
   :description "Simple and powerful background jobs"
   :url "https://github.com/b-ryan/farmhand"
   :deploy-repositories [["releases" :clojars]
